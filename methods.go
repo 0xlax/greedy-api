2dd20c48eadbb872e3167f4515521418bdfa8e00
0xlax <harshu8310@gmail.com>
2023-06-01 01:16:14 +0530

    feat: SET skeleton

--- methods.go
+++ methods.go
@@ -1,1 +1,13 @@
 package main
+
+import "time"
+
+// SET
+// Writes the value to the datastore using the key and according to the specified parameters.
+// Pattern: SET <key> <value> <expiry time>? <condition>?
+
+func (store *KeyValueStore) Set(key string, value string, expiryTime time.Time, condition string) (string, error) {
+	// one goroutine can access the critical section of code protected by the mutex at a time
+	store.mutex.Lock()
+	defer store.mutex.Unlock()
+}
