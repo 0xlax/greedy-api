e49e3e90596d8c6979d372ffb80a809380d65d6b
0xlax <harshu8310@gmail.com>
2023-06-01 19:01:44 +0530

    feat: Add GET function

--- methods.go
+++ methods.go
@@ -32,3 +32,37 @@ func (store *KeyValueStore) Set(key string, value string, expiryTime time.Time, 
 
 	return "OK", nil
 }
+
+// Get method to retrieve the value for a given key
+func (store *KeyValueStore) Get(key string) (string, error) {
+	store.mutex.RLock()
+	defer store.mutex.RUnlock()
+
+	if keyValue, ok := store.data[key]; ok {
+		if keyValue.expiryTime != nil && time.Now().After(*keyValue.expiryTime) {
+			delete(store.data, key)
+			return "", fmt.Errorf("key has expired")
+		}
+
+		return keyValue.value, nil
+	}
+
+	return "", fmt.Errorf("key not found")
+}
+
+// QPush method to append values to a queue
+func (store *KeyValueStore) QPush(key string, values ...string) {
+	store.mutex.Lock()
+	defer store.mutex.Unlock()
+
+	if _, ok := store.data[key]; !ok {
+		store.data[key] = &KeyValue{
+			value:      "",
+			expiryTime: nil,
+		}
+	}
+
+	for _, value := range values {
+		store.data[key].value += value + " "
+	}
+}
