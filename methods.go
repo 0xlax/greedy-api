0b5977f42e4b9ce83b928feb9d29d46f11a05b6b
0xlax <harshu8310@gmail.com>
2023-06-01 17:15:46 +0530

    update Set func

--- methods.go
+++ methods.go
@@ -1,6 +1,9 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // SET
 // Writes the value to the datastore using the key and according to the specified parameters.
@@ -10,4 +13,22 @@ func (store *KeyValueStore) Set(key string, value string, expiryTime time.Time, 
 	// one goroutine can access the critical section of code protected by the mutex at a time
 	store.mutex.Lock()
 	defer store.mutex.Unlock()
+
+	// Check the condition if the key already exists
+	if condition == "NX" {
+		if _, ok := store.data[key]; ok {
+			return "", fmt.Errorf("key already exists")
+		}
+	} else if condition == "XX" {
+		if _, ok := store.data[key]; !ok {
+			return "", fmt.Errorf("key does not exist")
+		}
+	}
+
+	store.data[key] = &KeyValue{
+		value:      value,
+		expiryTime: &expiryTime,
+	}
+
+	return "OK", nil
 }
