523332709e058bed67983d9dc7fff6a4df5eec5f
0xlax <harshu8310@gmail.com>
2023-06-02 12:49:58 +0530

    Fix ExpiryTime in SET

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"strconv"
 	"strings"
@@ -70,20 +69,14 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	case "BQPOP":
 		handleBQPOP(w, parts)
 	default:
-		sendErrorResponse(w, "unknown command")
+		sendErrorResponse(w, "invalid command")
 	}
 }
 
-// SET
-// Writes the value to the datastore using the key and according to the specified parameters.
-// Pattern: SET <key> <value> <expiry time>? <condition>?
-
-func SET(input string, store *KeyValueStore) (string, error) {
-	parts := strings.Split(input, " ")
-
-	// Validate the input format
-	if len(parts) < 3 || len(parts) > 5 {
-		return "", fmt.Errorf("invalid input format")
+func handleSET(w http.ResponseWriter, parts []string) {
+	if len(parts) < 3 {
+		sendErrorResponse(w, "invalid command format")
+		return
 	}
 
 	key := parts[1]
@@ -91,148 +84,190 @@ func SET(input string, store *KeyValueStore) (string, error) {
 	var expiryTime time.Time
 	var condition string
 
-	// Parse expiry time if provided
-	if len(parts) >= 4 {
-		if strings.HasPrefix(parts[3], "EX") {
-			seconds, err := strconv.Atoi(parts[3][2:])
-			if err != nil {
-				return "", fmt.Errorf("invalid expiry time")
-			}
-			expiryTime = time.Now().Add(time.Duration(seconds) * time.Second)
-		} else {
-			return "", fmt.Errorf("invalid input format")
+	if len(parts) >= 4 && strings.HasPrefix(parts[3], "EX") {
+		seconds, err := strconv.Atoi(parts[3][2:])
+		if err != nil {
+			sendErrorResponse(w, "invalid expiry time")
+			return
 		}
+		expiryTime = time.Now().Add(time.Duration(seconds) * time.Second)
 	}
 
-	// Parse condition if provided
 	if len(parts) == 5 {
-		condition = parts[4]
+		condition = strings.ToUpper(parts[4])
 		if condition != "NX" && condition != "XX" {
-			return "", fmt.Errorf("invalid condition")
+			sendErrorResponse(w, "invalid condition")
+			return
 		}
 	}
 
-	// one goroutine can access the critical section of code protected by the mutex at a time
 	store.mutex.Lock()
 	defer store.mutex.Unlock()
 
-	// Check the condition if the key already exists
 	if condition == "NX" {
-		if _, ok := store.data[key]; ok {
-			return "", fmt.Errorf("key already exists")
+		if _, ok := store.Data[key]; ok {
+			sendErrorResponse(w, "key already exists")
+			return
 		}
 	} else if condition == "XX" {
-		if _, ok := store.data[key]; !ok {
-			return "", fmt.Errorf("key does not exist")
+		if _, ok := store.Data[key]; !ok {
+			sendErrorResponse(w, "key does not exist")
+			return
 		}
 	}
 
-	store.data[key] = &KeyValue{
-		value:      value,
-		expiryTime: &expiryTime,
+	store.Data[key] = &KeyValue{
+		Value:      value,
+		ExpiryTime: &expiryTime,
 	}
 
-	return "OK", nil
+	sendOKResponse(w)
 }
 
-// Get method to retrieve the value for a given key
-func (store *KeyValueStore) Get(key string) (string, error) {
-	store.mutex.RLock()
-	defer store.mutex.RUnlock()
+func handleGET(w http.ResponseWriter, parts []string) {
+	if len(parts) != 2 {
+		sendErrorResponse(w, "invalid command format")
+		return
+	}
 
-	if keyValue, ok := store.data[key]; ok {
-		if keyValue.expiryTime != nil && time.Now().After(*keyValue.expiryTime) {
-			if keyValue.expiryTime.IsZero() {
-				delete(store.data, key)
-				return "", fmt.Errorf("key has expired")
+	key := parts[1]
+
+	store.mutex.Lock()
+	defer store.mutex.Unlock()
+
+	if kv, ok := store.Data[key]; ok {
+		sendValueResponse(w, kv.Value)
+		return
+	}
+
+	sendErrorResponse(w, "key not found")
+}
+
+func handleQPUSH(w http.ResponseWriter, parts []string) {
+	if len(parts) < 3 {
+		sendErrorResponse(w, "invalid command format")
+		return
+	}
+
+	key := parts[1]
+	values := parts[2:]
+
+	store.mutex.Lock()
+	defer store.mutex.Unlock()
+
+	if kv, ok := store.Data[key]; ok {
+		for _, value := range values {
+			kv.Value += " " + value
+		}
+	} else {
+		store.Data[key] = &KeyValue{
+			Value: strings.Join(values, " "),
+		}
+	}
+
+	sendOKResponse(w)
+}
+
+func handleQPOP(w http.ResponseWriter, parts []string) {
+	if len(parts) != 2 {
+		sendErrorResponse(w, "invalid command format")
+		return
+	}
+
+	key := parts[1]
+
+	store.mutex.Lock()
+	defer store.mutex.Unlock()
+
+	if kv, ok := store.Data[key]; ok {
+		values := strings.Split(kv.Value, " ")
+		if len(values) > 0 {
+			value := values[len(values)-1]
+			values = values[:len(values)-1]
+			kv.Value = strings.Join(values, " ")
+			sendValueResponse(w, value)
+			return
+		}
+	}
+
+	sendErrorResponse(w, "queue is empty")
+}
+
+func handleBQPOP(w http.ResponseWriter, parts []string) {
+	if len(parts) != 3 {
+		sendErrorResponse(w, "invalid command format")
+		return
+	}
+
+	key := parts[1]
+	timeout, err := strconv.ParseFloat(parts[2], 64)
+	if err != nil {
+		sendErrorResponse(w, "invalid timeout")
+		return
+	}
+
+	store.mutex.Lock()
+	kv, ok := store.Data[key]
+	store.mutex.Unlock()
+
+	if ok {
+		if timeout == 0 {
+			values := strings.Split(kv.Value, " ")
+			if len(values) > 0 {
+				value := values[len(values)-1]
+				values = values[:len(values)-1]
+				kv.Value = strings.Join(values, " ")
+				sendValueResponse(w, value)
+				return
+			}
+		} else if timeout > 0 {
+			ticker := time.NewTicker(time.Duration(timeout) * time.Second)
+			select {
+			case <-ticker.C:
+				sendErrorResponse(w, "timeout")
+				return
+			case <-time.After(1 * time.Second):
+				store.mutex.Lock()
+				kv, ok = store.Data[key]
+				store.mutex.Unlock()
+				if ok {
+					values := strings.Split(kv.Value, " ")
+					if len(values) > 0 {
+						value := values[len(values)-1]
+						values = values[:len(values)-1]
+						kv.Value = strings.Join(values, " ")
+						sendValueResponse(w, value)
+						return
+					}
+				}
 			}
 		}
-
-		return keyValue.value, nil
 	}
 
-	return "", fmt.Errorf("key not found")
+	sendErrorResponse(w, "queue is empty")
 }
 
-// QPush method to append values to a queue
-func (store *KeyValueStore) QPush(key string, values ...string) {
-	store.mutex.Lock()
-	defer store.mutex.Unlock()
-
-	if _, ok := store.data[key]; !ok {
-		store.data[key] = &KeyValue{
-			value:      "",
-			expiryTime: nil,
-		}
+func sendErrorResponse(w http.ResponseWriter, errorMessage string) {
+	response := ErrorResponse{
+		Error: errorMessage,
 	}
 
-	for _, value := range values {
-		store.data[key].value += value + " "
-	}
+	w.WriteHeader(http.StatusBadRequest)
+	json.NewEncoder(w).Encode(response)
 }
 
-// QPop method to retrieve the last inserted value from a queue
-func (store *KeyValueStore) QPop(key string) (string, error) {
-	store.mutex.Lock()
-	defer store.mutex.Unlock()
-
-	if keyValue, ok := store.data[key]; ok {
-		if keyValue.expiryTime != nil && time.Now().After(*keyValue.expiryTime) {
-			delete(store.data, key)
-			return "", fmt.Errorf("queue is empty")
-		}
-
-		values := strings.Fields(keyValue.value)
-		if len(values) > 0 {
-			lastValue := values[len(values)-1]
-			values = values[:len(values)-1]
-			store.data[key].value = strings.Join(values, " ")
-			return lastValue, nil
-		}
-
-		delete(store.data, key)
-		return "", fmt.Errorf("queue is empty")
+func sendValueResponse(w http.ResponseWriter, value string) {
+	response := ValueResponse{
+		Value: value,
 	}
 
-	return "", fmt.Errorf("key not found")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
 }
 
-// BQPop method to perform a blocking queue read operation
-func (store *KeyValueStore) BQPop(key string, timeout float64) (string, error) {
-	store.mutex.Lock()
-	defer store.mutex.Unlock()
+func sendOKResponse(w http.ResponseWriter) {
+	response := struct{}{}
 
-	// Check if the queue exists
-	if _, ok := store.data[key]; !ok {
-		return "", fmt.Errorf("queue does not exist")
-	}
-
-	queue := store.data[key]
-
-	// Check if the queue is empty
-	if len(strings.Fields(queue.value)) > 0 {
-		return store.QPop(key)
-	}
-
-	// If timeout is 0, return immediately without blocking
-	if timeout == 0 {
-		return "", nil
-	}
-
-	// Create a channel to wait for the value to be pushed
-	valueChan := make(chan string, 1)
-
-	// Start a goroutine to read from the channel after the specified timeout
-	go func() {
-		time.Sleep(time.Duration(timeout) * time.Second)
-		valueChan <- ""
-	}()
-
-	select {
-	case value := <-valueChan:
-		if value == "" {
-			return "", fmt.Errorf("timeout exceeded")
-		}
-		return value, nil
-	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
 }
