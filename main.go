e835c382bff56f32431021dd001892a73aacc7ac
0xlax <harshu8310@gmail.com>
2023-06-02 13:35:53 +0530

    Add comments

--- main.go
+++ main.go
@@ -48,8 +48,8 @@ func main() {
 }
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-	defer r.Body.Close()
+	decoder := json.NewDecoder(r.Body) //Decoder to decode request body into "Command" struct
+	defer r.Body.Close()               //Request body is closed after erquest is processed
 
 	var cmd Command
 	err := decoder.Decode(&cmd)
@@ -58,12 +58,12 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	parts := strings.Split(cmd.Command, " ")
+	parts := strings.Split(cmd.Command, " ") //Splits the command string into parts
 	if len(parts) == 0 {
 		sendErrorResponse(w, "invalid command")
 		return
 	}
-
+	//First index is converted to uppercase and performed a switch statement to trigger appropriate function.
 	switch strings.ToUpper(parts[0]) {
 	case "SET":
 		handleSET(w, parts)
@@ -74,7 +74,7 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	case "QPOP":
 		handleQPOP(w, parts)
 	case "BQPOP":
-		handleBQPOP(w, parts)
+		handleBQPOP(w, parts) //Optional
 	default:
 		sendErrorResponse(w, "invalid command")
 	}
